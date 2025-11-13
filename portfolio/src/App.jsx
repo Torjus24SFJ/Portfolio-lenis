059ccae2b4f4c99244d2bfe8b3f672e7f4681f1f
Torjus <Torjus24sfj@gmail.com>
2025-11-13 11:12:08 +0100

    header section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,23 +1,19 @@
 import "./App.css";
-// import StaggerBoxes from "./components/StaggerBoxes";
-// import HorizontalScroller from "./components/HorizontalScroller";
-// import TextRevealer from "./components/TextRevealer";
 
 function App() {
   
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="w-full max-w-7xl mx-auto px-4">
-          {/* <StaggerBoxes /> */}
+      <div className="wrapper w-screen h-screen bg-[url('/images/p6.png')] bg-repeat bg-auto">
+      <header className="w-screen h-20 sticky top-0">
+        <div className="flex items-center m-2">
+          <div className="grid grid-rows-2"></div>
+        <img src="../images/Logo-transparent-bg.png" className="w-10 h-10"></img>
+        <p className="m-4 font-light">Torjus Tveten</p>
         </div>
-      </div>
-      <div className="h-screen" />
-      {/* <HorizontalScroller /> */}
-      <div className="sm:text-2xl md:text-4xl xl:text-5xl font-bold flex justify-center m-12 uppercase">
-      {/* <TextRevealer font="font-monosans" text="This entire sentence reveals at once" /> */}
-      </div>
-      <div className="h-screen"></div>
+      </header>
+      <div className="flex justify-center"><p className="font-bold text-4xl mt-8">Front End Developer Portfolio</p></div>
+    </div>
     </>
   );
 }
